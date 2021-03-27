fb214c2a58f2b9e52c789ab68c3b83d0a1f89dc5
Anri Lombard <anri.m.lombard@gmail.com>
2021-03-27 05:44:30 +0200

    Docstring progress

--- Section5/c155.py
+++ Section5/c155.py
@@ -5,9 +5,27 @@
 
 import random
 
+
 def get_integer(prompt):
     """
+    Get an integer from Standard Input (stdin).
 
-    :param prompt:
-    :return:
+    The function will continue looping, and prompting the user, until
+    a valid `int` is entered.
+
+    :param prompt: The String that the user will see, when they're
+        prompted to enter the value.
+    :return: The integer that the user enters.
     """
+    while True:
+        temp = input(prompt)
+        if temp.isnumeric():
+            return int(temp)
+
+        print(f"{temp} is not a valid number")
+
+
+print(input.__doc__)
+print("*" * 80)
+print(get_integer.__doc__)
+print("*" * 80)
---
+++ Section5/learning.md
@@ -0,0 +1,7 @@
+# Modules
+- "A module is a file containing Python defenitions and statements.<br>
+  The file name is the module name with the suffix .py appended."
+  
+- Each .py file is a new Python module.
+
+- Modules can be imported into other modules or executed.
